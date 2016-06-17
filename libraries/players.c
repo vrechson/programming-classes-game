4c448669327a9977087774c7f34e8cc5139503d7
matheus vrech <whoismath@gmail.com>
2016-06-17 18:11:59 -0300

    Create players.c

---
+++ libraries/players.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "players.h"
+
+
+void buildPlayer(Players *player)
+{
+  char
+  printf("Hello Player! insert your name to start building your map: ");
+  scanf("%30s", player->name);
+//  printf("Ok %s, lets ");
+
+}
+void buildIA(Players *player)
+{
+
+}

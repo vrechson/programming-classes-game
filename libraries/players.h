b3a4250f9e23b1fc6fb56ccd4d234d06bc04b1cd
matheus vrech <whoismath@gmail.com>
2016-06-17 18:12:26 -0300

    Create players.h

---
+++ libraries/players.h
@@ -0,0 +1,16 @@
+/*
+ * This file must contain all functions related to the game player construction and manuitence.
+ */
+
+/*
+ *	Libraries
+ ***********************************************************************/
+#ifndef _PLAYERS_H_
+#define _PLAYERS_H_
+
+#include "definitions.h"
+
+void buildPlayer(Players *player);
+void buildIA(Players *player);
+
+#endif

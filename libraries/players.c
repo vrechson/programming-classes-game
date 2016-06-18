3a250702d2994fed5ce6163b422a1eb0cfb81b9a
matheus vrech <whoismath@gmail.com>
2016-06-18 00:55:44 -0300

    Update players.c

--- libraries/players.c
+++ libraries/players.c
@@ -3,23 +3,21 @@
 #include "players.h"
 
 
-void buildPlayer(Players *player, Players *player2)
+void buildPlayer(Players *player)
 {
   int i, j, k, x, y, flag;
   char answer;
-  printf("Hello Player! insert your name to start building your map: ");
+  printf("Vamo começar! Insira seu nome: ");
   scanf("%30s", player->name);
 
-  printf("Ok %s, lets set your board map! first step is positioning your %d submarine(s) (1 slot of space).\n", player->name, SUBMARINE);
+  printf("Certo %s, vamos configurar seu mapa! primeiro posicione seu(s) %d submarino(s) (1 casa de espaço).\n", player->name, SUBMARINE);
   for (i = 1, flag = 0; i <= SUBMARINE; i++, flag = 0) {
     do {
-      printf("please insert a line and column to set your %d submarine: ", i);
+      printf("por favor, insira a linha(letra) e a coluna(número) do seu %d submarino, respectvamente: ", i);
       scanf(" %c %d", &x, &y);
       flag = CheckDisponibility(&x, y, 1, 1, player);
     } while (flag);
-    printf("%c\n", player->map[y][x].presentation);
     Positioning(x, y, 1, 1, 'S', player);
-    printf("%c\n", player->map[y][x].presentation);
   }
   printf("Agora vamos posicionar seu(s) %d battleship(s).\n", BATTLESHIP);
   for (i = 1, flag = 0; i <= BATTLESHIP; i++, flag = 0) {
@@ -31,7 +29,7 @@ void buildPlayer(Players *player, Players *player2)
     } while (flag);
     Positioning(x, y, 2, 2, 'B', player);
   }
-  DrawBoard(player, player2);
+
   printf("O próximo passo é alocar seu(s) %d craiser(s).\n", CRAISER);
   for (i = 1, flag = 0; i <= CRAISER; i++, flag = 0) {
     do {
@@ -44,7 +42,7 @@ void buildPlayer(Players *player, Players *player2)
 
     Positioning(x, y, 1, 2, 'C', player);
   }
-  DrawBoard(player, player2);
+
   printf("Partimos então para seu(s) %d destroyer(s).\n", DESTROYER);
   for (i = 1, flag = 0; i <= DESTROYER; i++, flag = 0) {
     do {
@@ -57,7 +55,7 @@ void buildPlayer(Players *player, Players *player2)
 
     Positioning(x, y, 1, 4, 'D', player);
   }
-  DrawBoard(player, player2);
+
   printf("Para finalizar inserimos seu(s) %d aircraft(s).\n", AIRCRAFT);
   for (i = 1, flag = 0; i <= AIRCRAFT; i++, flag = 0) {
     do {

806eb1160f41a1aba961eeac4b793333edb7910b
matheus vrech <whoismath@gmail.com>
2016-06-18 14:44:58 -0300

    Update players.c

--- libraries/players.c
+++ libraries/players.c
@@ -1,37 +1,46 @@
+/*
+ *	Libraries
+ ***********************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "players.h"
 
 
-
+/*
+ *	Functions
+ ***********************************************************************/
 void buildPlayer(Players *player)
 {
-  int i, j, k, x, y, flag;
-  char answer;
+  int i, x, y, flag, next;
 
   system(CLEAR);
   printf("Vamos começar! Insira seu nome: ");
   scanf("%30s", player->name);
+  system(CLEAR);
+
+  if (SHOW_MAP)
+    DrawMap(player);
 
   printf("Certo %s, vamos configurar seu mapa! primeiro posicione seu(s) %d submarino(s) (1 casa de espaço).\n", player->name, SUBMARINE);
   for (i = 1, flag = 0; i <= SUBMARINE; i++, flag = 0) {
     do {
       printf("por favor, insira a linha(letra) e a coluna(número) do seu %d submarino, respectvamente: ", i);
       scanf(" %c %d", &x, &y);
-      flag = CheckDisponibility(&x, y, 1, 1, player);
+      flag = CheckDisponibility(&x, y, 1, 1, 0, player);
     } while (flag);
-    Positioning(x, y, 1, 1, 'S', player);
+    Positioning(x, y, 1, 1, 0, 'S', player);
   }
+
   printf("Agora vamos posicionar seu(s) %d battleship(s).\n", BATTLESHIP);
   for (i = 1, flag = 0; i <= BATTLESHIP; i++, flag = 0) {
     do {
       printf("Por favor, insira a linha e a coluna do seu %d battleship: ", i);
       scanf(" %c %d", &x, &y);
-      flag = CheckDisponibility(&x, y, 2, 2, player);
+      flag = CheckDisponibility(&x, y, 2, 2, 0, player);
 
     } while (flag);
-    Positioning(x, y, 2, 2, 'B', player);
+    Positioning(x, y, 2, 2, 0, 'B', player);
   }
 
   printf("O próximo passo é alocar seu(s) %d craiser(s).\n", CRAISER);
@@ -40,11 +49,11 @@ void buildPlayer(Players *player)
       printf("Por favor, insira a linha e a coluna do seu %d craiser: ", i);
       scanf(" %c %d", &x, &y);
 
-      flag = CheckDisponibility(&x, y, 1, 2, player);
+      flag = CheckDisponibility(&x, y, 1, 2, 0, player);
 
     } while (flag);
 
-    Positioning(x, y, 1, 2, 'C', player);
+    Positioning(x, y, 1, 2, 0, 'C', player);
   }
 
   printf("Partimos então para seu(s) %d destroyer(s).\n", DESTROYER);
@@ -53,11 +62,11 @@ void buildPlayer(Players *player)
       printf("Por favor, insira a linha e a coluna do seu %d destroyer: ", i);
       scanf(" %c %d", &x, &y);
 
-      flag = CheckDisponibility(&x, y, 1, 4, player);
+      flag = CheckDisponibility(&x, y, 1, 4, 0, player);
 
     } while (flag);
 
-    Positioning(x, y, 1, 4, 'D', player);
+    Positioning(x, y, 1, 4, 0, 'D', player);
   }
 
   printf("Para finalizar inserimos seu(s) %d aircraft(s).\n", AIRCRAFT);
@@ -66,11 +75,19 @@ void buildPlayer(Players *player)
       printf("Por favor, insira a linha e a coluna do seu %d aircraft: ", i);
       scanf(" %c %d", &x, &y);
 
-      flag = CheckDisponibility(&x, y, 1, 5, player);
+      flag = CheckDisponibility(&x, y, 1, 5, 0, player);
 
     } while (flag);
 
-    Positioning(x, y, 1, 5, 'A', player);
+    Positioning(x, y, 1, 5, 0, 'A', player);
+  }
+
+  next = 3;
+  while (next != 1) {
+    if (!next)
+      buildPlayer(player);
+    printf("Mapa finalizado, digite 1 para continuar e 0 para refazer seu tabuleiro: ");
+    scanf(" %d", &next);
   }
 
 }

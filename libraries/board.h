ff04a4423b5046b1ae0b7662cd0adcde0b15f77b
matheus vrech <whoismath@gmail.com>
2016-06-18 14:44:06 -0300

    Update board.h

--- libraries/board.h
+++ libraries/board.h
@@ -10,11 +10,14 @@
 
 #include "definitions.h"
 
+/*
+ *	Prototypes
+ ***********************************************************************/
 void DrawBoard(Players *player1, Players *player2);
 void DrawMap(Players *player);
 void InitializeMap(Players *player1, Players *player2);
 int Letter2Num(int *x);
-int CheckDisponibility(int *x, int y, int height, int width, Players *player);
-int Positioning(int x, int y, int height, int width, char style, Players *player);
+int CheckDisponibility(int *x, int y, int height, int width, int rotation, Players *player);
+void Positioning(int x, int y, int height, int width, int rotation, char style, Players *player);
 
 #endif

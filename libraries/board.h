1249807e0f03aee90ff9ccb5eaacbadf9b0ae351
matheus vrech <whoismath@gmail.com>
2016-06-19 03:25:02 -0300

    Update board.h

--- libraries/board.h
+++ libraries/board.h
@@ -17,10 +17,10 @@ void DrawBoard(Players *player1, Players *player2);
 void DrawMap(Players *player);
 void InitializeMap(Players *player);
 void HideThings(Players *player);
-int Letter2Num(int *x);
-int CheckDisponibility(int *x, int y, int height, int width, int rotation, int ia, Players *player);
+int Letter2Num(char x);
+int CheckDisponibility(int x, int y, int height, int width, int rotation, int ia, Players *player);
 void Positioning(int x, int y, int height, int width, int rotation, char style, Players *player);
-int PosBombing(int *x, int y, Players *player);
+int PosBombing(int x, int y, Players *player);
 int IABombing(Players *player);
 
 #endif

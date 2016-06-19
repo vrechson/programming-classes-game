8b7b3e1067a223fbfa72895fca3a4ca3cbd28527
Matheus Vrech <mvrech@lemonslab.lemonslab>
2016-06-19 02:25:53 -0300

    test

--- navalb.c
+++ navalb.c
@@ -1,10 +1,9 @@
 /*
- * navalb is a game developed in C
+ * navalb is a game developed in C as part of algorithm classes
  *
  * The point of the game is guess positions (e.g. A-12, or C-1) looking for parts of the enemy ships
  * the user that discover first all the ships parts of his enemy win the game.
  */
-
 
 /*
  *	Libraries
@@ -12,43 +11,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <time.h>
+#include <time.h>
 #include "libraries/board.h"
 #include "libraries/definitions.h"
 #include "libraries/players.h"
 
+/*
+ *	Main
+ ***********************************************************************/
 int main(int argc, char *argv[])
 {
-  Players player1, player2;
-  int players, i, j;
+  Players player1, player2, *winner;
+  int players;
 
-  //srand((unsigned) time(NULL));
+  srand((unsigned) time(NULL));
+
   system(CLEAR);
-
-  //DrawBoard(player1, player2);
-  InitializeMap(&player1, &player2);
-
-  printf("Welcome to %s version %.2f\n\n", argv[0], VERSION);
+  InitializeMap(&player1);
+  InitializeMap(&player2);
   DrawBoard(&player1, &player2);
 
+  printf("Bem vindo ao %s, versão %.2f\n\n", argv[0], VERSION);
+
   do {
-    printf("\n\nPlease, select the number of players(1/2): ");
-    scanf("%d", &players);
+    printf("\n\nPor favor, escolha o número de jogadores[1/2]: ");
+    scanf(" %d", &players);
     if (players < 1 || players > 2)
-      printf("Ok, you must choose one or two players, try again.\n");
+      printf("Você deve escolher entre um ou dois jogaroes, tente de novo.\n");
   } while (players < 1 || players > 2);
 
   if (players == 1) {
-    buildPlayer(&player1, &player2);
+  //  buildPlayer(&player1);
     buildIA(&player2);
+    DrawBoard(&player1, &player2);
+    DrawBoard(&player1, &player2);
+    winner = guessEngine(&player1, &player2, 1);
   } else {
-    buildPlayer(&player1, &player2);
-    buildPlayer(&player1, &player2);
+    buildPlayer(&player1);
+    buildPlayer(&player2);
+    DrawBoard(&player1, &player2);
+    winner = guessEngine(&player1, &player2, 0);
   }
 
-
-
-  memset(player1.map, WATER, MAP_SIZE * MAP_SIZE * sizeof(char));
-
+  printf(" %s ganhou o jogo! deseja jogar novamente? (y/n)", winner->name);
   return 0;
 }
